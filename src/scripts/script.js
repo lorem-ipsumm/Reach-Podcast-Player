07003e842f046963dd11155903f8df2066929ab8
SolarFloss <nicholasmrnickmorris@gmail.com>
2017-07-26 14:42:05 -0400

    Added marquee for long episode titles

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,17 +1,11 @@
-//const parser = require("rss-parser");
+var scroll = false;
+function start(e){
+  if(scroll){
+    e.getElementsByClassName("marquee")[0].classList.remove("marquee-scroll");
+    scroll = false;
+  }else{
+    e.getElementsByClassName("marquee")[0].classList.add("marquee-scroll");
+    scroll = true;
+  }
 
-
-/*
-parser.parseURL('http://feeds.gimletmedia.com/crimetownshow', function(err,parsed){
-  var audioLink = parsed.feed.entries[0].enclosure.url;
-  console.log(audioLink);
-
-
-  var sound = new Howl({
-    src: [audioLink],
-    html5: true
-  });
-
-  //sound.play();
-});
-*/
+}
