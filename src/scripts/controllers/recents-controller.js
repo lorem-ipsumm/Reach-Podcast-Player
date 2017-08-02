3068e241c30cbad3817926737ec889811112d66e
SolarFloss <nicholasmrnickmorris@gmail.com>
2017-08-01 23:56:36 -0400

    Now using Firebase to store podcast entered by users

--- src/scripts/controllers/recents-controller.js
+++ src/scripts/controllers/recents-controller.js
@@ -1,11 +1,34 @@
 angular.module('main').controller('recents-controller',function($scope,$rootScope){
+  $scope.database_entries = admin.database().ref("podcast-entries");
   $scope.listenQueue = [];
   $scope.clicked = false;
   $scope.edit = false;
 
-  $scope.add = function(){
-    $scope.recents.push({title: 'Cool Games Inc', author: 'Polygon'});
-  };
+
+  //Attempt to upload everything to the global databse on window close
+  window.onbeforeunload = function(evt){
+    $scope.database_entries.update($scope.local_database);
+  }
+
+
+
+  //Download the database upon startup
+  $scope.database_entries.once("value",function(snapshot){
+    $scope.local_database = snapshot.val();
+    console.log("database loaded");
+  });
+
+
+
+
+
+  $rootScope.$on('$routeChangeStart',function(scope, next, current){
+
+    fs.writeFile('./test-podcasts.txt',"um", function(err){
+      if(!err)
+        console.log("saved");
+    });
+  });
 
   $scope.addButtonClicked = function(){
     if(!$scope.clicked){
@@ -14,15 +37,14 @@ angular.module('main').controller('recents-controller',function($scope,$rootScop
       var text = angular.element(document.querySelector('#url-input'))[0].value;
       if(text.length > 0){
         //Try to get URL
-        add(text);
+        //console.log(text);
+        $scope.add(text);
       }else{
         $scope.clicked = false;
         $scope.edit = false;
       }
     }
   };
-
-
 
 
   $scope.imageClicked = function(title, author, image, desciption,entries){
@@ -75,7 +97,6 @@ angular.module('main').controller('recents-controller',function($scope,$rootScop
     var file = fs.readFileSync("./saved-podcasts.txt")
     var text = JSON.parse(file);
 
-
     //update each podcast's entry list
     for(var i = 0; i < text.length;i++){
       update(i,text);
@@ -96,10 +117,10 @@ angular.module('main').controller('recents-controller',function($scope,$rootScop
   $scope.keyDown = function(e){
     var key = e.key;
     if(key == "Enter"){
-      var text = angular.element(document.querySelector('#url-input'))[0].value;
+      var text = $scope.inputUrl;
       if(text.length > 0){
         //Try to get URL
-        add(text);
+        $scope.add(text);
       }else{
         $scope.clicked = false;
         $scope.edit = false;
@@ -108,45 +129,107 @@ angular.module('main').controller('recents-controller',function($scope,$rootScop
   };
 
 
+  //For database searching
+  $scope.ctrlDown = false;
+  $scope.filterKeyDown = function(e){
+    var key = e.key;
+    if(key == "Control")
+      $scope.ctrlDown = true;
 
-  //https://www.npr.org/rss/podcast.php?id=510289
+    //Ctrl+Enter
+    if(key == "Enter"){
+      if($scope.ctrlDown){
+
+        //If the podcast is in the local database add it to the recents list
+        if($scope.local_database[$scope.query.toLowerCase()] != undefined){
+          var data = $scope.local_database[$scope.query.toLowerCase()];
+          $scope.query = "";
+          $scope.add(data.url);
+        }else{
+          //Display some sort of mesage telling the user that they will need to manually add the url
+          console.log("Nothing found");
+        }
+
+
+      }
+    }
+
+  };
+
+  $scope.filterKeyUp = function(e){
+    var key = e.key;
+    if(key == "Control")
+      $scope.ctrlDown = false;
+  };
+
+
+
   var debounce = false;
-  function add(text){
+  $scope.add = function(text){
     $scope.edit = false;
     $scope.clicked = false;
-    angular.element(document.querySelector('#url-input'))[0].value = "";
+    $scope.inputUrl = "";
+    if(text.includes("https://") || text.includes("http://")){
+      parser.parseURL(text, function(err,parsed){
+        //console.log(text);
+        if(debounce == false){
+          debounce = true;
+          if(parsed.feed.itunes.image != undefined)
+            $scope.imageUrl = parsed.feed.itunes.image;
+          else
+            $scope.imageUrl = "./res/thumbnails/Radiolab.jpg";
 
-    parser.parseURL($scope.inputUrl, function(err,parsed){
-      if(debounce == false){
-        debounce = true;
-        if(parsed.feed.itunes.image != undefined)
-          $scope.imageUrl = parsed.feed.itunes.image;
-        else
-          $scope.imageUrl = "./res/thumbnails/Radiolab.jpg";
-        $scope.author = parsed.feed.itunes.author;
-        $scope.title = parsed.feed.title;
-        $scope.description = parsed.feed.description;
-        $scope.entries = parsed.feed.entries;
-        $scope.inputUrl;
 
-        $scope.recents.unshift({
-          'title': $scope.title,
-          'author': $scope.author,
-          'imageUrl': $scope.imageUrl,
-          'description': $scope.description,
-          'entries': $scope.entries,
-          'rssUrl': $scope.inputUrl
-        });
-        fs.writeFile('./saved-podcasts.txt',JSON.stringify($scope.recents), function(err){
-          if(!err)
-            console.log("saved");
-        });
-        //console.log($scope.recents[0]);
-        $scope.$apply();
-      }
+          $scope.author = parsed.feed.itunes.author;
+          $scope.title = parsed.feed.title;
+          $scope.description = parsed.feed.description;
+          $scope.entries = parsed.feed.entries;
 
-    });
 
+
+          //Add the new entry to the local database regardless if it's already there
+          var new_entry = {
+            'author': $scope.author,
+            'description': $scope.description,
+            'title': $scope.title,
+            'url': text
+          }
+          $scope.local_database[$scope.title.toLowerCase()] = new_entry;
+
+
+
+
+
+          $scope.recents.unshift({
+            'title': $scope.title,
+            'author': $scope.author,
+            'imageUrl': $scope.imageUrl,
+            'description': $scope.description,
+            'entries': $scope.entries,
+            'rssUrl': text
+          });
+
+
+          fs.writeFile('./saved-podcasts.txt',JSON.stringify($scope.recents), function(err){
+            if(!err)
+              console.log("saved");
+          });
+          $scope.$apply();
+
+        }
+
+
+      });
+    }
+    //This was Itunes Search API, but I would definitely run into API limits
+    /*else{
+      request('https://itunes.apple.com/search?term=' + text + '&country=US&media=podcast',function(error,response,body){
+        url = JSON.parse(body).results[0].feedUrl;
+        console.log(JSON.parse(body));
+        $scope.add(url);
+      });
+    }
+    */
     debounce = false;
   }
 
