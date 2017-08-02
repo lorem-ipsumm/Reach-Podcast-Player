edde2a6189e2350cf682a36559fdc7c04312a154
SolarFloss <nicholasmrnickmorris@gmail.com>
2017-08-02 16:45:12 -0400

    Podcast lengths now show up next to podcast names

--- src/scripts/controllers/podcast-view-controller.js
+++ src/scripts/controllers/podcast-view-controller.js
@@ -9,7 +9,45 @@ angular.module('main').controller('podcast-view',function($scope,$rootScope,$win
     $scope.entries = arg.entries;
   });
 
+  /*
+  $scope.test = function(index){
 
+    if(!$scope.entries[index].itunes.duration.includes(":")){
+      var value = $scope.entries[index].itunes.duration;
+      var d = Number(value);
+      var hour = Math.floor(d/3600);
+      var minute = Math.floor(d % 3600 / 60);
+      var second = Math.floor(d % 3600 % 60);
+      var minuteDisplay = minute;
+      var secondDisplay = second;
+      var hourDisplay = hour;
+
+      if(minute < 10)
+        minuteDisplay = "0" + minute;
+      else
+        minuteDisplay = minute;
+
+      if(second < 10)
+        secondDisplay = "0" + second;
+      else
+        secondDisplay = second;
+
+      if(hour < 10)
+        hourDisplay = "0" + hour;
+      else
+        hourDisplay = hour;
+
+
+      if(hour == 0)
+        console.log(minuteDisplay + ":" + secondDisplay);
+      else
+        console.log(hourDisplay + ":" + minuteDisplay + ":" + secondDisplay);
+    }else{
+      console.log($scope.entries[index].itunes);
+    }
+
+  }
+  */
 
 
   //Close window on escape
@@ -38,6 +76,55 @@ angular.module('main').controller('podcast-view',function($scope,$rootScope,$win
   }
 
 
+})
+
+
+//Dispalying the duration of each podcast
+.directive('convert',function($rootScope){
+  return{
+    link: function($scope,element,attr){
+      var index = $scope.$index;
+      if($scope.entries[index].itunes.duration != undefined){
+        if(!$scope.entries[index].itunes.duration.includes(":")){
+          var value = $scope.entries[index].itunes.duration;
+          var d = Number(value);
+          var hour = Math.floor(d/3600);
+          var minute = Math.floor(d % 3600 / 60);
+          var second = Math.floor(d % 3600 % 60);
+          var minuteDisplay = minute;
+          var secondDisplay = second;
+          var hourDisplay = hour;
+
+          if(minute < 10)
+            minuteDisplay = "0" + minute;
+          else
+            minuteDisplay = minute;
+
+          if(second < 10)
+            secondDisplay = "0" + second;
+          else
+            secondDisplay = second;
+
+          if(hour < 10)
+            hourDisplay = "0" + hour;
+          else
+            hourDisplay = hour;
+
+
+          if(hour == 0)
+            element[0].textContent = minuteDisplay + ":" + secondDisplay;
+          else
+            element[0].textContent = hourDisplay + ":" + minuteDisplay + ":" + secondDisplay;
+        }else{
+
+          element[0].textContent = $scope.entries[index].itunes.duration;
+
+        }
+      }
+
+    }
+
+  }
 })
 
 
